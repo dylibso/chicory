9cc3e19d38cfa6cf2b8b574611516393b0fca263
Benjamin Eckel <bhelx@simst.im>
2023-10-07 10:23:20 -0500

    Add some more context to wast2json failure

--- test-gen-plugin/src/main/java/com/dylibso/chicory/maven/TestGenMojo.java
+++ test-gen-plugin/src/main/java/com/dylibso/chicory/maven/TestGenMojo.java
@@ -204,6 +204,8 @@ public class TestGenMojo extends AbstractMojo {
         }
 
         if (ps.exitValue() != 0) {
+            System.err.println("wast2json exiting with:"+ ps.exitValue());
+            System.err.println(ps.getErrorStream().toString());
             throw new RuntimeException("Failed to execute wast2json program.");
         }
 
