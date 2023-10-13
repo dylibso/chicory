4c658d6e1a8f0c2d3beebf44008c353e7fc88de1
Benjamin Eckel <bhelx@simst.im>
2023-10-13 13:48:11 -0500

    Support call indirect

--- wasm/src/main/java/com/dylibso/chicory/wasm/types/Table.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/types/Table.java
@@ -1,14 +1,19 @@
 package com.dylibso.chicory.wasm.types;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
     private ElementType elementType;
     private long limitMin;
     private long limitMax;
+    private List<Integer> funcRefs;
 
     public Table(ElementType elementType, long limitMin, long limitMax) {
         this.elementType = elementType;
         this.limitMin = limitMin;
         this.limitMax = limitMax;
+        this.funcRefs = new ArrayList<>();
     }
 
     public ElementType getElementType() {
@@ -21,5 +26,13 @@ public class Table {
 
     public long getLimitMax() {
         return limitMax;
+    }
+
+    public void addFuncRef(int funcRef) {
+        this.funcRefs.add(funcRef);
+    }
+
+    public int getFuncRef(int index) {
+        return this.funcRefs.get(index);
     }
 }
