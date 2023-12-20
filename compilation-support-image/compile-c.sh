6672ee306e628fe82812f23c8ca82717427b211e
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-12-20 16:26:31 +0000

    cleanup

--- compilation-support-image/compile-c.sh
+++ compilation-support-image/compile-c.sh
@@ -7,6 +7,6 @@ done < "/dev/stdin"
 
 WASI_SDK_PATH=/opt/wasi-sdk
 
-/opt/wasi-sdk/bin/clang -O0 -g -o tmp.wasm tmp.c -nostartfiles -Wl,--no-entry -Wl,--export=run
+/opt/wasi-sdk/bin/clang -O0 -g -o tmp.wasm tmp.c -nostartfiles -Wl,--no-entry -Wl,--export=run > /dev/null 2> /dev/null
 
 cat tmp.wasm
--- compilation-support-image/compile-wat.sh
+++ compilation-support-image/compile-wat.sh
@@ -5,6 +5,6 @@ do
   echo "$line" >> tmp.wat
 done < "/dev/stdin"
 
-/opt/wabt/bin/wat2wasm tmp.wat -o tmp.wasm
+/opt/wabt/bin/wat2wasm tmp.wat -o tmp.wasm > /dev/null 2> /dev/null
 
 cat tmp.wasm
