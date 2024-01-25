e4ab4b7a616fec14c55d84382150bde20042e0d6
David M. Lloyd <david.lloyd@redhat.com>
2024-01-25 13:39:43 -0600

    Add controlled mutability to section definitions
    Allow section definitions to be programmatically created and populated. Deletion and random insertion are not supported, because section indices are expected to remain stable and are referenced from other sections.

--- wasm/src/main/java/com/dylibso/chicory/wasm/types/GlobalSection.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/types/GlobalSection.java
@@ -1,14 +1,30 @@
 package com.dylibso.chicory.wasm.types;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class GlobalSection extends Section {
     private final ArrayList<Global> globals;
 
-    public GlobalSection(Global[] globals) {
+    /**
+     * Construct a new, empty section instance.
+     */
+    public GlobalSection() {
+        this(new ArrayList<>());
+    }
+
+    /**
+     * Construct a new, empty section instance.
+     *
+     * @param estimatedSize the estimated number of globals to reserve space for
+     */
+    public GlobalSection(int estimatedSize) {
+        this(new ArrayList<>(estimatedSize));
+    }
+
+    private GlobalSection(ArrayList<Global> globals) {
         super(SectionId.GLOBAL);
-        this.globals = new ArrayList<>(List.of(globals));
+        this.globals = globals;
     }
 
     public Global[] globals() {
@@ -21,5 +37,18 @@ public class GlobalSection extends Section {
 
     public Global getGlobal(int idx) {
         return globals.get(idx);
+    }
+
+    /**
+     * Add a global variable definition to this section.
+     *
+     * @param global the global to add to this section (must not be {@code null})
+     * @return the index of the newly-added global
+     */
+    public int addGlobal(Global global) {
+        Objects.requireNonNull(global, "global");
+        int idx = globals.size();
+        globals.add(global);
+        return idx;
     }
 }
--- wasm/src/main/java/com/dylibso/chicory/wasm/types/MemorySection.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/types/MemorySection.java
@@ -1,14 +1,30 @@
 package com.dylibso.chicory.wasm.types;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class MemorySection extends Section {
     private final ArrayList<Memory> memories;
 
-    public MemorySection(Memory[] memories) {
+    /**
+     * Construct a new, empty section instance.
+     */
+    public MemorySection() {
+        this(1);
+    }
+
+    /**
+     * Construct a new, empty section instance.
+     *
+     * @param estimatedSize the estimated number of memories to reserve space for
+     */
+    public MemorySection(int estimatedSize) {
+        this(new ArrayList<>(estimatedSize));
+    }
+
+    private MemorySection(ArrayList<Memory> memories) {
         super(SectionId.MEMORY);
-        this.memories = new ArrayList<>(List.of(memories));
+        this.memories = memories;
     }
 
     public int memoryCount() {
@@ -17,5 +33,18 @@ public class MemorySection extends Section {
 
     public Memory getMemory(int idx) {
         return memories.get(idx);
+    }
+
+    /**
+     * Add a memory definition to this section.
+     *
+     * @param memory the memory to add to this section (must not be {@code null})
+     * @return the index of the newly-added memory
+     */
+    public int addMemory(Memory memory) {
+        Objects.requireNonNull(memory, "memory");
+        int idx = memories.size();
+        memories.add(memory);
+        return idx;
     }
 }
