e1538d193872015399767a98d97f0ef4d59a8817
David M. Lloyd <david.lloyd@redhat.com>
2024-01-26 12:09:37 -0600

    Fixes to `Table` model class (#199)
    Make the limits immutable. Use the maximum limit constant `0x1_0000_0000` when the upper limit is unspecified, and simplify comparisons accordingly.
    There is a class which represents limits; change `Table` to reuse it.

--- wasm/src/main/java/com/dylibso/chicory/wasm/types/Limits.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/types/Limits.java
@@ -1,19 +1,31 @@
 package com.dylibso.chicory.wasm.types;
 
 public class Limits {
-    private final int min;
-    private final int max;
+    public static final long LIMIT_MAX = 0x1_0000_0000L;
 
-    public Limits(int min, int max) {
-        this.min = min;
-        this.max = max;
+    private static final Limits UNBOUNDED = new Limits(0);
+
+    private final long min;
+    private final long max;
+
+    public Limits(long min) {
+        this(min, LIMIT_MAX);
     }
 
-    public int min() {
+    public Limits(long min, long max) {
+        this.min = Math.min(Math.max(0, min), LIMIT_MAX);
+        this.max = Math.min(Math.max(min, max), LIMIT_MAX);
+    }
+
+    public long min() {
         return min;
     }
 
-    public int max() {
+    public long max() {
         return max;
+    }
+
+    public static Limits unbounded() {
+        return UNBOUNDED;
     }
 }
