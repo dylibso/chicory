4b15b89375b78b35dd9ce5314c16d42a83f7bcd0
David M. Lloyd <david.lloyd@redhat.com>
2024-02-02 11:00:51 -0600

    Make `Memory` and `MemoryLimits` final and immutable (#222)
    * Add in documentation and spec references
    * Add in `shared` flag support from threads spec
    * Add `null`-checking

--- wasm/src/main/java/com/dylibso/chicory/wasm/types/Memory.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/types/Memory.java
@@ -1,12 +1,28 @@
 package com.dylibso.chicory.wasm.types;
 
-public class Memory {
-    private MemoryLimits memoryLimits;
+import java.util.Objects;
 
+/**
+ * A memory definition.
+ * <p>
+ * See <a href="https://webassembly.github.io/spec/core/syntax/modules.html#syntax-mem">Memories</a> for
+ * reference.
+ */
+public final class Memory {
+    private final MemoryLimits memoryLimits;
+
+    /**
+     * Construct a new instance.
+     *
+     * @param memoryLimits the memory limits (must not be {@code null})
+     */
     public Memory(MemoryLimits memoryLimits) {
-        this.memoryLimits = memoryLimits;
+        this.memoryLimits = Objects.requireNonNull(memoryLimits, "memoryLimits");
     }
 
+    /**
+     * {@return the defined memory limits}
+     */
     public MemoryLimits memoryLimits() {
         return memoryLimits;
     }
