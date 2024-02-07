39c6059f546ea8d9aac7fa49b5bcfec1e5125aa7
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-02-07 15:25:34 +0000

    [chore] Update the version in the Readme during release (#231)

---
+++ scripts/update-readme-version.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -euxo pipefail
+
+VERSION=${1}
+
+SED_CMD="sed"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_CMD="gsed"
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SED_CMD -i "s|^  <version>[^ ]*|  <version>${VERSION}</version>|" ${SCRIPT_DIR}/../README.md
+$SED_CMD -i "s|^implementation 'com.dylibso.chicory[^ ]*|implementation 'com.dylibso.chicory:runtime:${VERSION}'|" ${SCRIPT_DIR}/../README.md
