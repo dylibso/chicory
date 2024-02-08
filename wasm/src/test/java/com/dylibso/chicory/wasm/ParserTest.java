d6e8dd14cfecb123aad98a9a5b557c608b3eab10
Benjamin Eckel <bhelx@simst.im>
2024-02-08 15:16:10 -0600

    fix tests

--- wasm/src/test/java/com/dylibso/chicory/wasm/ParserTest.java
+++ wasm/src/test/java/com/dylibso/chicory/wasm/ParserTest.java
@@ -12,8 +12,13 @@ import com.dylibso.chicory.wasm.types.ExternalType;
 import com.dylibso.chicory.wasm.types.OpCode;
 import com.dylibso.chicory.wasm.types.SectionId;
 import com.dylibso.chicory.wasm.types.ValueType;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -117,42 +122,37 @@ public class ParserTest {
         }
     }
 
-    // @Test
-    //    public void shouldParseAllFiles() {
-    //        File compiledDir = new File("src/test/resources/compiled/");
-    //        File wasmDir = new File("src/test/resources/wasm/");
-    //
-    //        List<File> files = new ArrayList<>();
-    //        files.addAll(
-    //                Arrays.asList(
-    //                        compiledDir.listFiles(
-    //                                (ignored, name) -> name.toLowerCase().endsWith(".wasm"))));
-    //        files.addAll(
-    //                Arrays.asList(
-    //                        wasmDir.listFiles(
-    //                                (ignored, name) -> name.toLowerCase().endsWith(".wasm"))));
-    //
-    //        if (files.isEmpty()) {
-    //            throw new RuntimeException("Could not find files");
-    //        }
-    //
-    //        var logger = new SystemLogger();
-    //        for (var f : files) {
-    //            var parser = new Parser(logger);
-    //            try (InputStream is = new FileInputStream(f)) {
-    //                parser.parseModule(is);
-    //            } catch (Exception e) {
-    //                throw new RuntimeException(String.format("Failed to parse file %s", f), e);
-    //            }
-    //        }
-    //    }
+    @Test
+    public void shouldParseAllFiles() {
+        File compiledDir = new File("../wasm-corpus/src/test/resources/compiled/");
+
+        List<File> files = new ArrayList<>();
+        files.addAll(
+                Arrays.asList(
+                        compiledDir.listFiles(
+                                (ignored, name) -> name.toLowerCase().endsWith(".wasm"))));
+
+        if (files.isEmpty()) {
+            throw new RuntimeException("Could not find files");
+        }
+
+        var logger = new SystemLogger();
+        for (var f : files) {
+            var parser = new Parser(logger);
+            try (InputStream is = new FileInputStream(f)) {
+                parser.parseModule(is);
+            } catch (Exception e) {
+                throw new RuntimeException(String.format("Failed to parse file %s", f), e);
+            }
+        }
+    }
 
     @Test
     public void shouldSupportCustomListener() throws IOException {
         var logger = new SystemLogger();
         var parser = new Parser(logger);
 
-        try (InputStream is = getClass().getResourceAsStream("/wasm/code.wasm")) {
+        try (InputStream is = getClass().getResourceAsStream("/compiled/count_vowels.rs.wasm")) {
             parser.includeSection(SectionId.CUSTOM);
             parser.parse(
                     is,
@@ -167,16 +167,6 @@ public class ParserTest {
                     });
         }
     }
-
-    //    @Test
-    //    public void shouldParseAst() {
-    //        var parser = new Parser("src/test/resources/wasm/code.wasm");
-    //        var module = parser.parseModule();
-    //        var codeSection = module.getCodeSection();
-    //        var fbody = codeSection.getFunctionBodies()[0];
-    //        var ast = fbody.getAst();
-    //        ast.print();
-    //    }
 
     @Test
     public void shouldParseFloats() throws IOException {
