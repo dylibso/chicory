34b261d945ba3d65b39a2287b5dd9560714d44d7
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-17 12:31:28 +0100

    check on win

--- wabt/src/main/java/com/dylibso/chicory/wabt/Files.java
+++ wabt/src/main/java/com/dylibso/chicory/wabt/Files.java
@@ -1,21 +1,14 @@
 package com.dylibso.chicory.wabt;
 
 import static java.nio.file.Files.copy;
-import static java.nio.file.Files.createDirectory;
-import static java.nio.file.Files.getFileAttributeView;
-import static java.nio.file.Files.isSymbolicLink;
 import static java.nio.file.Files.walkFileTree;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.attribute.PosixFilePermissions;
 
 // TODO: refactor the little customizations done here in the wasi module
 public final class Files {
@@ -25,35 +18,6 @@ public final class Files {
         walkFileTree(
                 source,
                 new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                            throws IOException {
-                        if (isSymbolicLink(dir)) {
-                            return FileVisitResult.SKIP_SUBTREE;
-                        }
-
-                        Path directory = target.resolve(source.relativize(dir).toString());
-
-                        if (!directory.toString().equals("/")) {
-                            FileAttribute<?>[] attributes = new FileAttribute[0];
-                            var attributeView =
-                                    getFileAttributeView(dir, PosixFileAttributeView.class);
-                            if (attributeView != null) {
-                                var permissions = attributeView.readAttributes().permissions();
-                                var attribute = PosixFilePermissions.asFileAttribute(permissions);
-                                attributes = new FileAttribute[] {attribute};
-                            }
-
-                            try {
-                                createDirectory(directory, attributes);
-                            } catch (FileAlreadyExistsException e) {
-                                // already exists ignore
-                            }
-                        }
-
-                        return FileVisitResult.CONTINUE;
-                    }
-
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
                             throws IOException {
