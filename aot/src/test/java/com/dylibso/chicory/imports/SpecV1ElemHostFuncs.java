d8470d425281974eba9a9ea63df88b40f0c5fb55
David Phillips <david@acz.org>
2024-06-06 15:21:40 -0700

    Enable additional AOT tests

---
+++ aot/src/test/java/com/dylibso/chicory/imports/SpecV1ElemHostFuncs.java
@@ -0,0 +1,106 @@
+package com.dylibso.chicory.imports;
+
+import static com.dylibso.chicory.test.gen.SpecV1ElemTest.mInstance;
+import static com.dylibso.chicory.test.gen.SpecV1ElemTest.module1Instance;
+import static com.dylibso.chicory.test.gen.SpecV1ElemTest.module4Instance;
+
+import com.dylibso.chicory.runtime.GlobalInstance;
+import com.dylibso.chicory.runtime.HostGlobal;
+import com.dylibso.chicory.runtime.HostImports;
+import com.dylibso.chicory.runtime.HostTable;
+import com.dylibso.chicory.runtime.TableInstance;
+import com.dylibso.chicory.wasm.types.Limits;
+import com.dylibso.chicory.wasm.types.Table;
+import com.dylibso.chicory.wasm.types.Value;
+import com.dylibso.chicory.wasm.types.ValueType;
+
+public class SpecV1ElemHostFuncs {
+
+    public static HostImports fallback() {
+        return new HostImports();
+    }
+
+    public static HostImports testModule3() {
+        return new HostImports(
+                new HostTable[] {
+                    new HostTable(
+                            "spectest",
+                            "table",
+                            new TableInstance(new Table(ValueType.FuncRef, new Limits(1))))
+                });
+    }
+
+    public static HostImports testModule5() {
+        return new HostImports(
+                new HostTable[] {
+                    new HostTable(
+                            "spectest",
+                            "table",
+                            new TableInstance(new Table(ValueType.FuncRef, new Limits(10))))
+                });
+    }
+
+    public static HostImports testModule6() {
+        return new HostImports(
+                new HostGlobal[] {
+                    new HostGlobal("spectest", "global_i32", new GlobalInstance(Value.i32(123)))
+                });
+    }
+
+    public static HostImports testModule7() {
+        return new HostImports(
+                new HostGlobal[] {
+                    new HostGlobal("spectest", "global_i32", new GlobalInstance(Value.i32(321)))
+                });
+    }
+
+    public static HostImports testModule11() {
+        return testModule5();
+    }
+
+    public static HostImports testModule13() {
+        return testModule3();
+    }
+
+    public static HostImports testModule16() {
+        return testModule3();
+    }
+
+    public static HostImports testModule17() {
+        return testModule3();
+    }
+
+    public static HostImports testModule18() {
+        return testModule5();
+    }
+
+    public static HostImports testModule19() {
+        return testModule5();
+    }
+
+    public static HostImports testModule23() {
+        return testModule5();
+    }
+
+    private static HostTable module1SharedTable() {
+        return new HostTable("module1", "shared-table", module1Instance.table(0));
+    }
+
+    public static HostImports module2() {
+        return new HostImports(new HostTable[] {module1SharedTable()});
+    }
+
+    public static HostImports module3() {
+        return new HostImports(new HostTable[] {module1SharedTable()});
+    }
+
+    public static HostImports testModule28() {
+        return new HostImports(
+                new HostTable[] {new HostTable("exporter", "table", mInstance.table(0))});
+    }
+
+    public static HostImports testModule30() {
+        return new HostImports(
+                new HostGlobal[] {new HostGlobal("module4", "f", module4Instance.global(0))});
+    }
+}
