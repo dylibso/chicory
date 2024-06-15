c2e75d230d8aa9417ef2eddc0ed887994fc76689
David Phillips <david@acz.org>
2024-06-15 10:54:41 +0800

    Implement control flow for AOT

--- aot/src/main/java/com/dylibso/chicory/aot/AotContext.java
+++ aot/src/main/java/com/dylibso/chicory/aot/AotContext.java
@@ -1,15 +1,20 @@
 package com.dylibso.chicory.aot;
 
+import static com.dylibso.chicory.aot.AotUtil.jvmType;
 import static com.dylibso.chicory.aot.AotUtil.slotCount;
+import static com.dylibso.chicory.aot.AotUtil.stackSize;
 
 import com.dylibso.chicory.aot.AotUtil.StackSize;
 import com.dylibso.chicory.wasm.types.FunctionBody;
 import com.dylibso.chicory.wasm.types.FunctionType;
+import com.dylibso.chicory.wasm.types.Instruction;
 import com.dylibso.chicory.wasm.types.ValueType;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Class for tracking context relevant to compiling a single function
@@ -27,7 +32,9 @@ final class AotContext {
     private final int memorySlot;
     private final int instanceSlot;
     private final int tempSlot;
-    private final Deque<StackSize> stackSizes = new ArrayDeque<>();
+    private final Deque<Deque<StackSize>> stackSizesStack = new ArrayDeque<>();
+    private final Map<Instruction, Integer> scopeStackSize = new HashMap<>();
+    private final Deque<Deque<StackSize>> restoreStackSize = new ArrayDeque<>();
 
     public AotContext(
             String internalClassName,
@@ -69,6 +76,7 @@ final class AotContext {
 
         this.slots = List.copyOf(slots);
         this.tempSlot = slot;
+        this.stackSizesStack.push(new ArrayDeque<>());
     }
 
     public String internalClassName() {
@@ -116,14 +124,54 @@ final class AotContext {
     }
 
     public Deque<StackSize> stackSizes() {
-        return stackSizes;
+        return stackSizesStack.getFirst();
     }
 
     public void pushStackSize(StackSize size) {
-        stackSizes.push(size);
+        stackSizes().push(size);
     }
 
     public StackSize popStackSize() {
-        return stackSizes.pop();
+        return stackSizes().pop();
+    }
+
+    public Deque<Deque<StackSize>> stackSizesStack() {
+        return stackSizesStack;
+    }
+
+    public void pushStackSizesStack() {
+        stackSizesStack.push(new ArrayDeque<>(stackSizes()));
+    }
+
+    public void popStackSizesStack() {
+        stackSizesStack.pop();
+    }
+
+    public void enterScope(Instruction scope, FunctionType scopeType) {
+        scopeStackSize.put(scope, stackSizes().size());
+
+        // stack sizes when exiting "polymorphic" blocks after unconditional control transfer
+        Deque<StackSize> stack = new ArrayDeque<>(stackSizes());
+        for (int i = 0; i < scopeType.params().size(); i++) {
+            stack.pop();
+        }
+        for (ValueType type : scopeType.returns()) {
+            stack.push(stackSize(jvmType(type)));
+        }
+        restoreStackSize.push(stack);
+    }
+
+    public void exitScope(Instruction scope) {
+        scopeStackSize.remove(scope);
+        restoreStackSize.pop();
+    }
+
+    public int scopeStackSize(Instruction scope) {
+        return scopeStackSize.get(scope);
+    }
+
+    public void scopeRestoreStackSize() {
+        stackSizesStack.pop();
+        stackSizesStack.push(restoreStackSize.getFirst());
     }
 }
