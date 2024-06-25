e16413393d5a196605bd59697eb6de5f9a9ecc1d
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-06-25 10:50:32 +0100

    Implement binaryleb128.wast malformed assertions (#404)
    Implementation of the malformed exceptions for Binary LEB128

--- wasm/src/main/java/com/dylibso/chicory/wasm/Encoding.java
+++ wasm/src/main/java/com/dylibso/chicory/wasm/Encoding.java
@@ -5,22 +5,30 @@ import java.nio.ByteBuffer;
 
 public final class Encoding {
 
+    // https://webassembly.github.io/spec/core/binary/values.html#integers
+    public static final int MAX_VARINT_LEN_32 = 5; // ceil(32/7)
+    public static final int MAX_VARINT_LEN_64 = 10; // ceil(64/7)
+
     /**
      * Reads an unsigned integer from {@code byteBuffer}.
      */
-    public static long readUnsignedLeb128(ByteBuffer byteBuffer) {
+    public static long readUnsignedLeb128(ByteBuffer byteBuffer, int maxVarInt) {
         long result = 0;
         int shift = 0;
+        int i = 0;
         while (true) {
+            i++;
             if (byteBuffer.remaining() == 0) {
                 throw new MalformedException("unexpected end");
             }
-
             byte b = byteBuffer.get();
             result |= (long) (b & 0x7F) << shift;
 
             if ((b & 0x80) == 0) {
                 break;
+            }
+            if (i >= maxVarInt) {
+                throw new MalformedException("integer representation too long");
             }
 
             shift += 7;
@@ -35,14 +43,19 @@ public final class Encoding {
     public static long readSigned32Leb128(ByteBuffer byteBuffer) {
         long result = 0;
         int shift = 0;
+        int i = 0;
         byte currentByte;
 
         do {
+            i++;
             if (byteBuffer.remaining() == 0) {
-                throw new IllegalArgumentException("SLEB128 reached the end of the buffer");
+                throw new MalformedException("unexpected end");
             }
 
             currentByte = byteBuffer.get();
+            if ((currentByte & 0x80) != 0 && i >= MAX_VARINT_LEN_32) {
+                throw new MalformedException("integer representation too long");
+            }
             result |= (long) (currentByte & 0x7F) << shift;
             shift += 7;
         } while ((currentByte & 0x80) != 0);
@@ -58,15 +71,20 @@ public final class Encoding {
     public static long readSigned64Leb128(ByteBuffer byteBuffer) {
         long result = 0;
         int shift = 0;
+        int i = 0;
         byte currentByte;
         int size = 64; // 64 bits for i64
 
         do {
+            i++;
             if (byteBuffer.remaining() == 0) {
-                throw new IllegalArgumentException("LEB128 reached the end of the buffer");
+                throw new MalformedException("unexpected end");
             }
 
             currentByte = byteBuffer.get();
+            if ((currentByte & 0x80) != 0 && i >= MAX_VARINT_LEN_64) {
+                throw new MalformedException("integer representation too long");
+            }
             result |= (long) (currentByte & 0x7F) << shift;
             shift += 7;
         } while ((currentByte & 0x80) != 0);
@@ -76,20 +94,10 @@ public final class Encoding {
             result |= -1L << shift;
         }
 
-        return result;
-    }
+        if (i >= MAX_VARINT_LEN_64 && currentByte != 0 && currentByte < 0x7F) {
+            throw new MalformedException("integer too large");
+        }
 
-    /**
-     * Computes the number of bytes required to encode a given value as LEB128
-     * @param value
-     * @return
-     */
-    public static int computeLeb128Size(int value) {
-        int size = 0;
-        do {
-            size++;
-            value >>>= 7;
-        } while (value != 0);
-        return size;
+        return result;
     }
 }
