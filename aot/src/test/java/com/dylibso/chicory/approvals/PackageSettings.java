e9bc18538d32c098c85b9fd25f586fdcf689aa38
David Phillips <david@acz.org>
2024-07-03 19:29:53 -0700

    Remove extra directory for approvals

--- aot/src/test/java/com/dylibso/chicory/approvals/PackageSettings.java
+++ aot/src/test/java/com/dylibso/chicory/approvals/PackageSettings.java
@@ -1,6 +1,5 @@
 package com.dylibso.chicory.approvals;
 
 public class PackageSettings {
-    public String UseApprovalSubdirectory = "approvals";
     public String ApprovalBaseDirectory = "../resources";
 }
