d994ba4e0eac160f79591c46d773ab95824b3e10
David Phillips <david@acz.org>
2024-08-15 12:22:52 -0700

    Enable all tests for AOT

---
+++ aot/src/test/java/com/dylibso/chicory/imports/SpecV1TokensHostFuncs.java
@@ -0,0 +1,23 @@
+package com.dylibso.chicory.imports;
+
+import com.dylibso.chicory.runtime.HostFunction;
+import com.dylibso.chicory.runtime.HostImports;
+import com.dylibso.chicory.runtime.Instance;
+import com.dylibso.chicory.wasm.types.Value;
+import java.util.List;
+
+public class SpecV1TokensHostFuncs {
+    public static HostImports fallback() {
+        return HostImports.builder()
+                .addFunction(
+                        new HostFunction(
+                                (Instance instance, Value... args) -> {
+                                    return null;
+                                },
+                                "spectest",
+                                "print",
+                                List.of(),
+                                List.of()))
+                .build();
+    }
+}
