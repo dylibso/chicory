f206a3decd199520a07f14ce4ab21eea76988f04
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-09-17 13:59:09 +0200

    Remove `@Deprecated`
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- runtime/src/main/java/com/dylibso/chicory/runtime/HostFunction.java
+++ runtime/src/main/java/com/dylibso/chicory/runtime/HostFunction.java
@@ -7,20 +7,6 @@ import java.util.List;
  * A HostFunction is an ExternalFunction that has been defined by the host.
  */
 public class HostFunction extends ExternalFunction {
-    /**
-     * @deprecated use {@link HostFunction#HostFunction(String, String, WasmFunctionHandle,
-     * List, List)}
-     */
-    @Deprecated
-    public HostFunction(
-            WasmFunctionHandle handle,
-            String moduleName,
-            String fieldName,
-            List<ValueType> paramTypes,
-            List<ValueType> returnTypes) {
-        this(moduleName, fieldName, handle, paramTypes, returnTypes);
-    }
-
     public HostFunction(
             String moduleName,
             String fieldName,
