eea6e02d3ff39944e2eaf4f0e57fd111b42e6bc1
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-10-03 17:01:16 +0100

    More initial docs

--- scripts/update-readme-version.sh
+++ scripts/update-readme-version.sh
@@ -12,3 +12,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 $SED_CMD -i "s|^  <version>[^ ]*|  <version>${VERSION}</version>|" ${SCRIPT_DIR}/../README.md
 $SED_CMD -i "s|^implementation 'com.dylibso.chicory[^ ]*|implementation 'com.dylibso.chicory:runtime:${VERSION}'|" ${SCRIPT_DIR}/../README.md
+
+$SED_CMD -i "s|^  <version>[^ ]*|  <version>${VERSION}</version>|" ${SCRIPT_DIR}/../docs/docs/index.md
+$SED_CMD -i "s|^implementation 'com.dylibso.chicory[^ ]*|implementation 'com.dylibso.chicory:runtime:${VERSION}'|" ${SCRIPT_DIR}/../docs/docs/index.md
