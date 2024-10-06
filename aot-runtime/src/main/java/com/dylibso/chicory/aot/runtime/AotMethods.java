46d7dcade9e668d30adf3fb2ee97a2759df42ec2
David Phillips <david@acz.org>
2024-10-06 12:23:56 -0700

    Remove redundant cast in AotMethods

--- aot-runtime/src/main/java/com/dylibso/chicory/aot/runtime/AotMethods.java
+++ aot-runtime/src/main/java/com/dylibso/chicory/aot/runtime/AotMethods.java
@@ -31,7 +31,7 @@ public final class AotMethods {
     }
 
     public static int tableGet(int index, int tableIndex, Instance instance) {
-        return (int) OpcodeImpl.TABLE_GET(instance, tableIndex, index);
+        return OpcodeImpl.TABLE_GET(instance, tableIndex, index);
     }
 
     public static void tableSet(int index, int value, int tableIndex, Instance instance) {
