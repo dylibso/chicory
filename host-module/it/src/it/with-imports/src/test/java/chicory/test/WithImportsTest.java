ed7b9b449b1e7ea36c6026e369334551eb639691
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-12-02 17:37:11 +0000

    finalizing generation

--- host-module/it/src/it/with-imports/src/test/java/chicory/test/WithImportsTest.java
+++ host-module/it/src/it/with-imports/src/test/java/chicory/test/WithImportsTest.java
@@ -1,48 +1,58 @@
 package chicory.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.dylibso.chicory.experimental.hostmodule.annotations.HostModule;
+import com.dylibso.chicory.experimental.hostmodule.annotations.WasmExport;
 import com.dylibso.chicory.experimental.hostmodule.annotations.WasmModuleInterface;
+import com.dylibso.chicory.runtime.ImportValues;
+import com.dylibso.chicory.runtime.Instance;
+import com.dylibso.chicory.wasm.Parser;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
 
 class WithImportsTest {
     public final AtomicInteger count = new AtomicInteger();
 
+    @HostModule("console")
     @WasmModuleInterface("host-function.wat.wasm")
-    class TestModule {}
+    class TestModule
+            implements TestModule_ModuleExports, TestModule_ModuleImports, TestModule_Console {
+        private static final String EXPECTED = "Hello, World!";
+        private final Instance instance;
 
-    //    @HostModule("console")
-    //    class TestModule {
-    //        private static final String EXPECTED = "Hello, World!";
-    //        private final Instance instance;
-    //
-    //        public TestModule() {
-    //            instance =
-    //                    Instance.builder(
-    //                                    Parser.parse(
-    //                                            WithImportsTest.class.getResourceAsStream(
-    //                                                    "/compiled/host-function.wat.wasm")))
-    //                            .withImportValues(
-    //                                    ImportValues.builder()
-    //                                            .addFunction(
-    //
-    // TestModule_ModuleFactory.toHostFunctions(this))
-    //                                            .build())
-    //                            .build();
-    //        }
-    //
-    //        public void logIt() {
-    //            instance.export("logIt").apply();
-    //        }
-    //
-    //        @WasmExport
-    //        public void log(int len, int offset) {
-    //            var message = instance.memory().readString(offset, len);
-    //
-    //            if (EXPECTED.equals(message)) {
-    //                count.incrementAndGet();
-    //            }
-    //        }
-    //    }
+        public TestModule() {
+            var module =
+                    Parser.parse(
+                            WithImportsTest.class.getResourceAsStream(
+                                    "/compiled/host-function.wat.wasm"));
+            var imports =
+                    ImportValues.builder()
+                            .addFunction(TestModule_ModuleFactory.toHostFunctions(this))
+                            .build();
+            instance = Instance.builder(module).withImportValues(imports).build();
+        }
+
+        @Override
+        public Instance instance() {
+            return instance;
+        }
+
+        @Override
+        public TestModule_Console console() {
+            return this;
+        }
+
+        @Override
+        @WasmExport
+        public void log(int len, int offset) {
+            var message = instance.memory().readString(offset, len);
+
+            if (EXPECTED.equals(message)) {
+                count.incrementAndGet();
+            }
+        }
+    }
 
     @Test
     public void withImportsModule() {
@@ -50,9 +60,9 @@ class WithImportsTest {
         var withImportsModule = new TestModule();
 
         // Act
-        //        withImportsModule.logIt();
+        withImportsModule.logIt();
 
         // Assert
-        //        assertEquals(10, count.get());
+        assertEquals(10, count.get());
     }
 }
