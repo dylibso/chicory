9a53caaf71308c02a88cc8e07cc279daf7f59c74
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-12-20 10:30:23 +0000

    Wasm module annotation (low level/C-like bindings) (#690)
    Opening to hear feedback before I stabilize the implementation with
    proper tests etc. etc.
    
    Here, as discussed a few times, I'm generating low level(C-like)
    bindings to give type safe interfaces to the user.
    
    The diff itself is not very interesting, but I encourage to test out the
    behavior locally, this is what I got running it on OPA (make sure to
    open the `OpaWasm` file, and, locally, the files generated by the
    annotations):
    
    https://github.com/StyraInc/opa-java-wasm/compare/main...andreaTP:try-wasm-module
    
    This implementation is not very opinionated, and there is space for a
    more seamless integration(leaving to the user less wiring), but is a
    good "toolbox" approach to the problem imho.
    
    The `WasmModuleInterface` annotation generates 3 things (examples from
    OPA):
    
    - `_ModuleExports` an interface that holds `default` implementation of
    the Wasm module export section, i.e.:
    ```java
    public interface OpaWasm_ModuleExports {
    
        Instance instance();
    
        default Memory memory() {
            return instance().memory();
        }
    
        default int opaEvalCtxNew() {
            long result = instance().export("opa_eval_ctx_new").apply()[0];
            return (int) result;
        }
    
        default int opaMalloc(int arg0) {
            long result = instance().export("opa_malloc").apply((long) arg0)[0];
            return (int) result;
        }
    
        default void opaEvalCtxSetInput(int arg0, int arg1) {
            instance().export("opa_eval_ctx_set_input").apply((long) arg0,
                                                              (long) arg1);
            return;
        }
       ...
    ```
    - `_ModuleImports` maps the low level module imports and group them by
    module name, e.g.:
    ```java
    public interface OpaWasm_ModuleImports {
        OpaWasm_Env env();
    }
    ```
    - the actual `ModuleImports` interfaces, i.e.:
    ```java
    public interface OpaWasm_Env {
        int opaBuiltin0(int arg0, int arg1);
        int opaBuiltin1(int arg0, int arg1, int arg2);
        ...
    ```
    
    I'm not sure the interaction with `HostModule` is ideal (but I might be
    overlooking it).
    I'm pretty happy with the `ModuleExports` encoding and it saves a lot of
    upfront work when dealing with a new wasm file.
    The `ModuleImports` section acts more as an implementation guideline and
    might benefit from being more opinionated.
    Happy to hear feedback to finalize the PR!

---
+++ host-module/it/src/it/import-exports/src/test/java/chicory/test/ImportExportsTest.java
@@ -0,0 +1,59 @@
+package chicory.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.dylibso.chicory.experimental.hostmodule.annotations.WasmModuleInterface;
+import com.dylibso.chicory.runtime.Instance;
+import com.dylibso.chicory.wasm.Parser;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Test;
+
+class ImportExportsTest {
+    public final AtomicInteger count = new AtomicInteger();
+
+    @WasmModuleInterface("host-function.wat.wasm")
+    class TestModule
+            implements TestModule_ModuleExports, TestModule_ModuleImports, TestModule_Console {
+        private static final String EXPECTED = "Hello, World!";
+        private final Instance instance;
+
+        public TestModule() {
+            var module =
+                    Parser.parse(
+                            ImportExportsTest.class.getResourceAsStream("/host-function.wat.wasm"));
+
+            instance = Instance.builder(module).withImportValues(toImportValues()).build();
+        }
+
+        @Override
+        public Instance instance() {
+            return instance;
+        }
+
+        @Override
+        public TestModule_Console console() {
+            return this;
+        }
+
+        @Override
+        public void log(int len, int offset) {
+            var message = instance.memory().readString(offset, len);
+
+            if (EXPECTED.equals(message)) {
+                count.incrementAndGet();
+            }
+        }
+    }
+
+    @Test
+    public void withImportsModule() {
+        // Arrange
+        var withImportsModule = new TestModule();
+
+        // Act
+        withImportsModule.logIt();
+
+        // Assert
+        assertEquals(10, count.get());
+    }
+}
