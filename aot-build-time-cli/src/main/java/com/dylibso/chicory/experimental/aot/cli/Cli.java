667df3518605d69d3bf7858ae26e8fe9d486c291
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2025-03-06 11:22:39 +0000

    Split the precompiled AOT logic to a separate jar (#784)
    This is the agreed split of the precompiled AOT logic that have been
    discussed.
    
    cc. @illarionov

---
+++ aot-build-time-cli/src/main/java/com/dylibso/chicory/experimental/aot/cli/Cli.java
@@ -0,0 +1,81 @@
+package com.dylibso.chicory.experimental.aot.cli;
+
+import com.dylibso.chicory.experimental.build.time.aot.Config;
+import com.dylibso.chicory.experimental.build.time.aot.Generator;
+import com.dylibso.chicory.wasm.Version;
+import java.io.IOException;
+import java.nio.file.Path;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "chicory-aot",
+        versionProvider = Cli.VersionProvider.class,
+        mixinStandardHelpOptions = true,
+        helpCommand = true,
+        header = "A CLI to generate resources using the Chicory AOT compiler")
+public class Cli implements Runnable {
+    static final class VersionProvider implements CommandLine.IVersionProvider {
+        @Override
+        public String[] getVersion() {
+            return new String[] {Version.version()};
+        }
+    }
+
+    @CommandLine.Parameters(arity = "1", description = "The wasm file to be used")
+    Path wasmFile;
+
+    @CommandLine.Option(
+            order = 1,
+            names = "--prefix",
+            description = "The prefix to be used to generate resources",
+            defaultValue = "com.dylibso.chicory.Wasm")
+    String prefix;
+
+    @CommandLine.Option(
+            order = 2,
+            names = "--source-dir",
+            description = "The target folder to use for source files",
+            defaultValue = ".")
+    Path targetSourceFolder;
+
+    @CommandLine.Option(
+            order = 3,
+            names = "--class-dir",
+            description = "The target folder to use for class files",
+            defaultValue = ".")
+    Path targetClassFolder;
+
+    @CommandLine.Option(
+            order = 4,
+            names = "--wasm-dir",
+            description = "The target folder to use for the wasm meta file",
+            defaultValue = ".")
+    Path targetWasmFolder;
+
+    @Override
+    public void run() {
+        var config =
+                Config.builder()
+                        .withWasmFile(wasmFile)
+                        .withName(prefix)
+                        .withTargetClassFolder(targetClassFolder)
+                        .withTargetSourceFolder(targetSourceFolder)
+                        .withTargetWasmFolder(targetWasmFolder)
+                        .build();
+
+        var generator = new Generator(config);
+
+        try {
+            generator.generateMetaWasm();
+            generator.generateResources();
+            generator.generateSources();
+        } catch (IOException e) {
+            throw new CommandLine.PicocliException("Failed to execute the command", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Cli()).execute(args);
+        System.exit(exitCode);
+    }
+}
---
+++ aot-build-time/src/main/java/com/dylibso/chicory/experimental/build/time/aot/Config.java
@@ -0,0 +1,124 @@
+package com.dylibso.chicory.experimental.build.time.aot;
+
+import java.nio.file.Path;
+import java.util.StringJoiner;
+
+public final class Config {
+    /**
+     * the wasm module to be used
+     */
+    private final Path wasmFile;
+
+    /**
+     * the base name to be used for the generated classes
+     */
+    private final String name;
+
+    /**
+     * the target folder to generate classes
+     */
+    private final Path targetClassFolder;
+
+    /**
+     * the target source folder to generate the java source code Machine implementation
+     */
+    private final Path targetSourceFolder;
+
+    /**
+     * the target wasm folder to generate the meta, stripped, wasm module
+     */
+    private final Path targetWasmFolder;
+
+    private Config(
+            Path wasmFile,
+            String name,
+            Path targetClassFolder,
+            Path targetSourceFolder,
+            Path targetWasmFolder) {
+        this.wasmFile = wasmFile;
+        this.name = name;
+        this.targetClassFolder = targetClassFolder;
+        this.targetSourceFolder = targetSourceFolder;
+        this.targetWasmFolder = targetWasmFolder;
+    }
+
+    public Path wasmFile() {
+        return wasmFile;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Path targetClassFolder() {
+        return targetClassFolder;
+    }
+
+    public Path targetSourceFolder() {
+        return targetSourceFolder;
+    }
+
+    public Path targetWasmFolder() {
+        return targetWasmFolder;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @SuppressWarnings("StringSplitter")
+    public String getPackageName() {
+        var split = name.split("\\.");
+        StringJoiner packageName = new StringJoiner(".");
+        for (int i = 0; i < split.length - 1; i++) {
+            packageName.add(split[i]);
+        }
+        return packageName.toString();
+    }
+
+    @SuppressWarnings("StringSplitter")
+    public String getBaseName() {
+        var split = name.split("\\.");
+        return split[split.length - 1];
+    }
+
+    public static final class Builder {
+        private Path wasmFile;
+        private String name;
+        private Path targetClassFolder;
+        private Path targetSourceFolder;
+        private Path targetWasmFolder;
+
+        private Builder() {}
+
+        public Builder withWasmFile(Path wasmFile) {
+            this.wasmFile = wasmFile;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withTargetClassFolder(Path targetClassFolder) {
+            this.targetClassFolder = targetClassFolder;
+            return this;
+        }
+
+        public Builder withTargetSourceFolder(Path targetSourceFolder) {
+            this.targetSourceFolder = targetSourceFolder;
+            return this;
+        }
+
+        public Builder withTargetWasmFolder(Path targetWasmFolder) {
+            this.targetWasmFolder = targetWasmFolder;
+            return this;
+        }
+
+        public Config build() {
+            return new Config(
+                    wasmFile, name, targetClassFolder, targetSourceFolder, targetWasmFolder);
+        }
+    }
+}
