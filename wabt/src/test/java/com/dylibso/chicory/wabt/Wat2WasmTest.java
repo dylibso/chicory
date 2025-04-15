54a962634dedb22c6ee87744b2d1b1dbee1e8f2b
Hiram Chirino <hiram@hiramchirino.com>
2025-04-15 17:07:45 -0400

    tests: add tests to verify that we can generate a large wasm from a wat file.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- wabt/src/test/java/com/dylibso/chicory/wabt/Wat2WasmTest.java
+++ wabt/src/test/java/com/dylibso/chicory/wabt/Wat2WasmTest.java
@@ -9,6 +9,14 @@ import com.dylibso.chicory.runtime.Instance;
 import com.dylibso.chicory.wasi.WasiExitException;
 import com.dylibso.chicory.wasm.Parser;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.junit.jupiter.api.Test;
 
 public class Wat2WasmTest {
@@ -50,5 +58,51 @@ public class Wat2WasmTest {
                                                 "src/test/resources/utf8-invalid-encoding-spec.0.wat")));
 
         assertEquals(1, exitException.exitCode());
+    }
+
+    @Test
+    public void canCompile50kFunctions() throws IOException {
+        var funcCount = 50_000;
+        buildBigWasm(funcCount, 0);
+    }
+
+    @Test
+    public void canCompileBigFunctions() throws IOException {
+        var funcCount = 10;
+        buildBigWasm(funcCount, 15_000);
+    }
+
+    public static final class Context {
+        public final ArrayList<Integer> functions = new ArrayList<>();
+        public final ArrayList<Integer> instructions = new ArrayList<>();
+    }
+
+    private byte[] buildBigWasm(int funcCount, int funcSize) {
+        var ctx = new Context();
+        for (int i = 0; i < funcCount; i++) {
+            ctx.functions.add(i + 1);
+        }
+        for (int i = 0; i < funcSize; i++) {
+            ctx.instructions.add(i + 1);
+        }
+
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        velocityEngine.setProperty(
+                "classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        velocityEngine.init();
+
+        Template t = velocityEngine.getTemplate("/big.wat");
+
+        VelocityContext context = new VelocityContext();
+        context.put("functions", ctx.functions);
+        context.put("instructions", ctx.instructions);
+
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
+        writer.flush();
+        String wat = writer.toString();
+
+        return Wat2Wasm.parse(wat);
     }
 }
