4cb4cf40bff50eb32236a7d7f5c5448f7e4c3e72
andreatp <andrea.peruffo1982@gmail.com>
2025-07-25 16:07:48 +0100

    annotation support for externref

--- wasm/src/test/java/com/dylibso/chicory/wasm/types/ValTypeTest.java
+++ wasm/src/test/java/com/dylibso/chicory/wasm/types/ValTypeTest.java
@@ -1,5 +1,8 @@
 package com.dylibso.chicory.wasm.types;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.dylibso.chicory.wasm.Parser;
 import org.junit.jupiter.api.Test;
 
 public class ValTypeTest {
@@ -37,5 +40,18 @@ public class ValTypeTest {
             ValType roundTrip = ValType.builder().fromId(id).build(this::context);
             assert vt.equals(roundTrip) : "Failed to roundtrip: " + vt;
         }
+    }
+
+    @Test
+    public void checkExternRef() {
+        var module =
+                Parser.parse(
+                        ValTypeTest.class.getResourceAsStream(
+                                "/compiled/externref-example.wat.wasm"));
+
+        assertEquals(3, module.typeSection().types().length);
+
+        var type0 = module.typeSection().types()[0].returns().get(0);
+        assertEquals(ValType.TypeIdxCode.EXTERN.code(), type0.typeIdx());
     }
 }
