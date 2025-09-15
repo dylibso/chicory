540b34f18b4056c6adfa0aa8028e3de3b4813cc2
A248 <a248-dev@tuta.io>
2025-09-16 00:43:47 +0300

    Add requires on objectweb.asm.util

--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.dylibso.chicory.compiler {
     requires transitive com.dylibso.chicory.wasm;
     requires org.objectweb.asm;
     requires org.objectweb.asm.commons;
+    requires org.objectweb.asm.util;
 
     exports com.dylibso.chicory.compiler;
     exports com.dylibso.chicory.compiler.internal to
