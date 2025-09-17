bef314b654b1548d4614bba4b20fc35d9e0063a4
A248 <a248-dev@tuta.io>
2025-09-17 13:51:07 +0300

    Add requires for zerofs

--- wabt/src/main/java/module-info.java
+++ wabt/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.dylibso.chicory.wabt {
     requires com.dylibso.chicory.runtime;
     requires com.dylibso.chicory.wasi;
     requires com.dylibso.chicory.wasm;
+    requires io.roastedroot.zerofs.ZeroFs;
 
     exports com.dylibso.chicory.wabt;
 }
